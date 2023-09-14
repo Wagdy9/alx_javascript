f265ceebe1d12f7dcaba231049de82cd34451f71
= <=>
2023-09-14 21:12:20 +0300

    Full

--- javascript_advanced/0-welcome.js
+++ javascript_advanced/0-welcome.js
@@ -1,4 +1,3 @@
-// a function welcome, takes two arguments, firstName (string) and lastName (string). It contains a variable named fullName that contains the concatenation of firstName and lastName. Finally, within it is a function named displayFullName that displays an alert with message 'Welcome, ' followed by the value of fullName.
 
 function welcome(firstName, lastName) {
     const fullName = firstName + ' ' + lastName;
