93199b3cea3d027c682116d1421ba2fd781aa9c4
= <=>
2023-09-29 13:43:45 +0300

    Full

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 
-
-import { Rectangle } from './4-rectangle';
+const Rectangle = require("./4-rectangle");
 
 class Square extends Rectangle {
-    constructor(size) {
-        super(size, size)
-    }
+  constructor(size) {
+    super(size, size);
+  }
 }
 
-export default Square;
+module.exports = Square;
